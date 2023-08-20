ab3e37d5e587357909ad193329e33da4743ec481
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-08-20 05:16:17 -0700

    added initial prototypes

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+unsigned int binane(unsigned int b);
+int ma_rahctup(char c);
+int _printf(const char *format, ...);
+int putsca(char *c);
+
+#endif
