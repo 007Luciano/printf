3393d09bfdbdb7f5f0c5f2733f0284cee1a7ac56
FrancisAmenya <amenyafrancis@gmail.com>
2023-08-21 13:25:58 -0700

    Main test case

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(void)
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
 	_printf("%b\n", 98);
-	printf("%b\n", 98);
+	/** printf("%b\n", 98); **/
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
@@ -40,8 +40,8 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");
+	/** _printf("Unknown:[%r]\n"); **/
+	/** printf("Unknown:[%r]\n"); **/
 
 	return (0);
 }
