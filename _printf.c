397aadb0039ba5d73130284ffb419975c61fe729
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-08-25 19:50:54 -0700

    revert back to _printf

--- _printf.c
+++ _printf.c
@@ -8,77 +8,59 @@ int _printf(const char *format, ...);
 
 int _printf(const char *format, ...)
 {
-	unsigned int i, count2, count = 0;
+unsigned int i, s_count, count = 0;
 
-	va_list args;
+va_list args;
 
-	va_start(args, format);
+va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
+if (format == NULL)
+return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			putinchar(format[i]);
-		}
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+putinchar(format[i]);
+}
 
-		else if (format[i + 1] == 'c')
-		{
-			putinchar(va_arg(args, int));
-			i++;
-		}
+else if (format[i + 1] == 'c')
+{
+putinchar(va_arg(args, int));
+i++;
+}
 
-		else if (format[i + 1] == 's')
-		{
-		const char *strin = va_arg(args, char *);
+else if (format[i + 1] == 's')
+{
+s_count = putsca(va_arg(args, char *));
 
-		if (strin == NULL)
-		{
-		char nullStrnull[] = "(null)";
+i++;
+count += (s_count - 1);
+}
 
-		int z;
+else if (format[i + 1] == '%')
+{
+putinchar('%');
+i++;
+}
 
-		for (z = 0; nullStrnull[z] != '\0'; z++)
-		{
-		putinchar(nullStrnull[z]);
-		count++;
-		}
-		}
-
-		else
-		{
-		count2 = putsca(va_arg(args, char *));
-		i++;
-		count += (count2 - 1);
-		}
-		i++;
-		}
-
-		else if (format[i + 1] == '%')
-		{
-			putinchar('%');
-			i++;
-		}
-
-		else if (format[i + 1] == 'b')
-		{
-			binane(va_arg(args, unsigned int));
-			i++;
-		}
-		else if (format[i + 1] == 'd')
-		{
-		decimal_con(va_arg(args, int));
-		i++;
-		}
-		else if (format[i + 1] == 'i')
-		{
-		decimal_con(va_arg(args, int));
-		i++;
-		}
-		count += 1;
-	}
+else if (format[i + 1] == 'b')
+{
+binane(va_arg(args, unsigned int));
+i++;
+}
+else if (format[i + 1] == 'd')
+{
+decimal_con(va_arg(args, int));
+i++;
+}
+else if (format[i + 1] == 'i')
+{
+decimal_con(va_arg(args, int));
+i++;
+}
+count += 1;
+}
 va_end(args);
 return (count);
 }
